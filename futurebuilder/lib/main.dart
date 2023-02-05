5df9d5f088d1bc4eb893560069a79148623f84d7
Emre Cem Aksu <emrecemaksu@hotmail.com>
2023-02-05 13:03:25 +0100

    Future Builder

---
+++ futurebuilder/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title = 'Future Builder Örneği';
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: Anaekran(),
+    );
+  }
+}
+
+class Anaekran extends StatefulWidget {
+  const Anaekran({super.key});
+  @override
+  State<Anaekran> createState() => _AnaekranState();
+}
+
+class _AnaekranState extends State<Anaekran> {
+  final Future<String> _waiting =
+      Future.delayed(const Duration(seconds: 2), () => 'Data Loaded');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: DefaultTextStyle(
+      style: Theme.of(context).textTheme.displayMedium!,
+      textAlign: TextAlign.center,
+      child: FutureBuilder<String>(
+        future: _waiting,
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              const Icon(
+                Icons.check_circle_outline,
+                color: Colors.green,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Result: ${snapshot.data}'),
+              ),
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ];
+          } else {
+            children = const <Widget>[
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Awaiting result...'),
+              ),
+            ];
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: children,
+            ),
+          );
+        },
+      ),
+    ));
+  }
+}
